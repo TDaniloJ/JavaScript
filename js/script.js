02c1550ce2de715268a2cd51b9b8ee0f5f739071
Danilo Dias <131262817+TDaniloJ@users.noreply.github.com>
2024-02-28 11:13:35 -0300

    codigo de estudo
    codigo simples de estudo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
+//alert("Ola Mundo!");
 
+document.write("<h3>Olá Mundo!</h3>");
+document.write("<h2>Exemplo da aula</h2>");
+document.write("<h2>Exemplo da aula3</h2>");
+
+// Função Logo a baixor
+
+var a, b, c;
+
+a = "Oi";
+a = 34;
+b = a + 2.5;
+c = b + "5";
+
+var soma = a *= 5;
+
+document.write(typeof(a), " ", typeof(b)," ", typeof(c));
+document.write("<br>" + a);
+document.write("<br>" + b);
+document.write("<br>" + c);
+document.write("<br>" + soma);
+
+console.log('Teste,testasadasassadasassasdfaaagdsdsgsdag');
+console.log('Todos os Direitos Reservados');
+
+
+var e = 1;
+var d, res = 3;
+document.write(++e);
+document.write(e++);
+document.write(e);
+
+d = e + 2;
+
+document.write("<br> b: ", d);
+res += 3;
+document.write("<br> res: ", res);
